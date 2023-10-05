c161db28418ae88ef3c783cf7a971a39f4f70e46
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2023-10-05 14:03:31 +0000

    Create HTML files and import images, started work

---
+++ index.js
@@ -0,0 +1,21 @@
+var submitButton;
+var startingLPForm;
+var endingLPForm;
+
+var divider;
+
+document.addEventListener("DOMContentLoaded", function(){
+    divider = document.getElementById("formDiv");
+    submitButton = document.getElementById("submit");
+    startingLPForm = document.getElementById("startAmount");
+    endingLPForm = document.getElementById("endAmount");
+});
+
+function submitForm() {
+    alert(startingLPForm.value + " " + endingLPForm.value);
+    var aaa = document.createElement("a");
+    aaa.innerText = "womp womp";
+    aaa.href = "display.html#start=" + startingLPForm.value + "&end=" + endingLPForm.value;
+    document.body.appendChild(aaa);
+    aaa.click();
+}
