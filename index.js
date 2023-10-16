c840ddff3d121a1e58b107e36d4242c04b7e5dd4
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2023-10-16 13:15:47 +0000

    hoping this works :)

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ var endingLPForm;
 
 var divider;
 
+var fakeSubmit;
+
 document.addEventListener("DOMContentLoaded", function(){
     divider = document.getElementById("formDiv");
     submitButton = document.getElementById("submit");
     startingLPForm = document.getElementById("startAmount");
     endingLPForm = document.getElementById("endAmount");
+    fakeSubmit = document.getElementById("submitButton");
 });
 
 function submitForm() {
     var aaa = document.createElement("a");
     aaa.innerText = "womp womp";
     aaa.href = "display.html#start=" + startingLPForm.value + "&end=" + endingLPForm.value;
+    alert(aaa.href);
     document.body.appendChild(aaa);
     aaa.click();
 }
